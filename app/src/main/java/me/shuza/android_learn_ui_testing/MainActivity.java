f0e925cb9a93c3700cb9a6ddc95a515cc54a0b34
shuza <Islam.zahid@aci-bd.com>
2017-12-07 17:31:01 +0600

    Simple activity with counter is added

--- app/src/main/java/me/shuza/android_learn_ui_testing/MainActivity.java
+++ app/src/main/java/me/shuza/android_learn_ui_testing/MainActivity.java
@@ -1,13 +1,28 @@
 package me.shuza.android_learn_ui_testing;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.btnSimpleTestActivity)
+    Button btnSimpleTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSimpleTestActivity)
+    public void startSimpleTestActivity(View view) {
+        startActivity(new Intent(MainActivity.this, SimpleTestActivity.class));
     }
 }
--- app/src/main/java/me/shuza/android_learn_ui_testing/SimpleTestActivity.java
+++ app/src/main/java/me/shuza/android_learn_ui_testing/SimpleTestActivity.java
@@ -2,12 +2,28 @@ package me.shuza.android_learn_ui_testing;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class SimpleTestActivity extends AppCompatActivity {
+    @BindView(R.id.tvCounterResult)
+    TextView tvCounterResult;
+
+    int count = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simple_test);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnIncreaseCounter)
+    public void increaseCounter(View view) {
+        tvCounterResult.setText(String.valueOf(++count));
     }
 }
