064d46fb8f7dad6c1e93580d14782b6517b3350b
shuza <Islam.zahid@aci-bd.com>
2017-12-07 17:44:06 +0600

    Test method for valid counter result show is added

--- app/src/androidTest/java/me/shuza/android_learn_ui_testing/CounterActivityInstrumentationTest.java
+++ app/src/androidTest/java/me/shuza/android_learn_ui_testing/CounterActivityInstrumentationTest.java
@@ -2,7 +2,14 @@ package me.shuza.android_learn_ui_testing;
 
 import android.support.test.rule.ActivityTestRule;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 import org.junit.Rule;
+import org.junit.Test;
 
 /**
  * :=  created by:  Shuza
@@ -17,4 +24,12 @@ public class CounterActivityInstrumentationTest {
     @Rule
     public ActivityTestRule<CounterActivity> activityTestRule = new ActivityTestRule<CounterActivity>(CounterActivity.class);
 
+    @Test
+    public void validateCounterResultTextView() {
+        int count = 0;
+        onView(withId(R.id.btnIncreaseCounter)).perform(click());
+        onView(withId(R.id.tvCounterResult)).check(matches(withText(String.valueOf(++count))));
+        onView(withId(R.id.btnIncreaseCounter)).perform(click());
+        onView(withId(R.id.tvCounterResult)).check(matches(withText(String.valueOf(++count))));
+    }
 }
